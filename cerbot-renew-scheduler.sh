337c0e826fd35f07f37a496f6f71bccc969bf09b
codexian codexian <codexian.ltd@gmail.com>
2019-03-10 04:01:03 +0530

    bash script update

--- cerbot-renew-scheduler.sh
+++ cerbot-renew-scheduler.sh
@@ -1,5 +1,8 @@
 set -x
-export $(egrep -v '^#' .env | xargs)
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export $(egrep -v '^#' $DIR/.env | xargs)
 
 
 URL="https://api.mailgun.net/v3/$MG_DOMAIN/messages"
